b2a0a3dbacc00444b881764704289a08d951d1b4
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2022-03-15 20:05:04 -0300

    feat: miragejs config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useState } from 'react';
 
+import { createServer, Model } from 'miragejs';
 import { ThemeProvider } from 'styled-components';
 
 import { Content, ContentTitle } from './components/common/Content';
@@ -8,6 +9,60 @@ import { SwitchThemeButton } from './components/SwitchThemeButton';
 import { GlobalStyle } from './styles/global';
 import dark from './styles/themes/dark';
 import light from './styles/themes/light';
+
+const questions = [
+  {
+    questionText: 'What is the capital of France?',
+    answerOptions: [
+      { answerText: 'New York', isCorrect: false },
+      { answerText: 'London', isCorrect: false },
+      { answerText: 'Paris', isCorrect: true },
+      { answerText: 'Dublin', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'Who is CEO of Tesla?',
+    answerOptions: [
+      { answerText: 'Jeff Bezos', isCorrect: false },
+      { answerText: 'Elon Musk', isCorrect: true },
+      { answerText: 'Bill Gates', isCorrect: false },
+      { answerText: 'Tony Stark', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'The iPhone was created by which company?',
+    answerOptions: [
+      { answerText: 'Apple', isCorrect: true },
+      { answerText: 'Intel', isCorrect: false },
+      { answerText: 'Amazon', isCorrect: false },
+      { answerText: 'Microsoft', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'How many Harry Potter books are there?',
+    answerOptions: [
+      { answerText: '1', isCorrect: false },
+      { answerText: '4', isCorrect: false },
+      { answerText: '6', isCorrect: false },
+      { answerText: '7', isCorrect: true },
+    ],
+  },
+];
+
+createServer({
+  models: {
+    question: Model,
+  },
+  seeds(server) {
+    server.db.loadData({
+      questions,
+    });
+  },
+  routes() {
+    this.namespace = 'api';
+    this.get('/questions', () => this.schema.all('question'));
+  },
+});
 
 function App() {
   const [theme, setTheme] = useState(dark);
